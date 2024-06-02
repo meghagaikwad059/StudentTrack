fc0c9a401526ddab33807c1e45367fda0b3bffe7
Megha Gaikwad <140902417+meghagaikwad059@users.noreply.github.com>
2024-06-02 11:18:26 +0530

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,12 @@
+create database crud;
+use crud;
+CREATE TABLE `student` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+select * from student;
+
